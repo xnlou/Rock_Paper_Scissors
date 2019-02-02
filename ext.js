197964b7f7ede3a7534592842055aac957406317
xnlou <xnlouey@gmail.com>
2019-02-02 16:12:37 -0600

    created game loop that tallies results and declares winner

--- ext.js
+++ ext.js
@@ -9,11 +9,12 @@ function computerPlay() {
 
 // Function to give game outcomes for user and computer input
 function playRound(playerSelection,computerSelection){
-    let result;
+    let result = "";
     let draw = "Draw, try again!";
     playerSelection = playerSelection.toString().toLowerCase();
 
     switch(computerSelection){
+        // rock  cases
         case "rock":
             if (playerSelection == "rock"){
                 result = draw;
@@ -25,6 +26,7 @@ function playRound(playerSelection,computerSelection){
                 result = "Oops...can you try again?";
             }
             break;
+        // paper cases
         case "paper":
             if (playerSelection == "rock"){
                 result = "You Lose! Paper beats rock.";
@@ -36,6 +38,7 @@ function playRound(playerSelection,computerSelection){
                 result = "Oops...can you try again?";
             }
             break;
+        // scissor cases
         case "scissors":
             if (playerSelection == "rock"){
                 result = "You Win! Rock beats scissors.";
@@ -49,4 +52,42 @@ function playRound(playerSelection,computerSelection){
             break;
     }
     return result;
+}
+
+// Function to simulate 5 rounds of RPS
+function game(){
+    let playerScore = 0;
+    let computerScore= 0;
+    let msg = "";
+    let msg_fin = "";
+    
+    // loop to play 5 games
+    for (var i=1;i<6;i++){
+        let playerSelection = prompt("Choose RPS!").toLowerCase();
+        let computerSelection = computerPlay();
+        result = playRound(playerSelection,computerSelection);
+        console.log("Round " + i + ": " + result);
+
+    // Keep track of digital score
+        if(result.includes("Win")){
+            playerScore += 1;
+            msg = "You win this round!";
+        } else if (result.includes("Lose")){
+            computerScore += 1;
+            msg = "You lose this round :(";
+        } else {
+            msg = "Draw!"
+        }
+    }
+
+    // Calculate final outcome message
+    if (playerScore > computerScore){
+        msg_fin = "You won the series!"
+    }   else if (computerScore > playerScore){
+        msg_fin = "You lost the series."
+    }   else{
+        msg_fin = "Draw...really?"
+    }
+    // display scores with message
+console.log("Your score: " + playerScore + "\ncomputerScore:" + computerScore + "\n" + msg_fin);
 }
