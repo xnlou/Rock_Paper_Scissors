14fb3bccb24718439b1c31d48cf16615bd6b5765
xnlou <xnlouey@gmail.com>
2019-03-05 14:51:00 -0600

    added event listeners and styling for GUI interface

--- ext.js
+++ ext.js
@@ -1,93 +1,83 @@
 // Javascript extension
+let playScore = 0;
+let compScore = 0;
+let container = document.querySelector('.container');
+let log = document.querySelector('#log');
+let result = document.createElement('h1');
+log.textContent = "First to score 5 wins! Choose your move!";
 
 // Function to randomly choose between RPS for computer player
 function computerPlay() {
-    moves = ['rock','paper','scissors'];
+    moves = ['Rock','Paper','Scissors'];
     randMove = Math.floor(Math.random()*3); //returns index of 1 , 2 or 3
     return moves[randMove];
 }
 
-// Function to give game outcomes for user and computer input
-function playRound(playerSelection,computerSelection){
-    let result = "";
-    let draw = "Draw, try again!";
-    playerSelection = playerSelection.toString().toLowerCase();
-
-    switch(computerSelection){
-        // rock  cases
-        case "rock":
-            if (playerSelection == "rock"){
-                result = draw;
-            } else if (playerSelection == "paper"){
-                result = "You Win!  Paper beats rock.";
-            } else if (playerSelection == "scissors"){
-                result = "You Lose!  Rock beats scissors";
-            }  else {
-                result = "Oops...can you try again?";
-            }
-            break;
-        // paper cases
-        case "paper":
-            if (playerSelection == "rock"){
-                result = "You Lose! Paper beats rock.";
-            } else if (playerSelection == "paper"){
-                result = draw;
-            } else if (playerSelection == "scissors"){
-                result = "You Win! Scissors beats paper.";
-            } else {
-                result = "Oops...can you try again?";
-            }
-            break;
-        // scissor cases
-        case "scissors":
-            if (playerSelection == "rock"){
-                result = "You Win! Rock beats scissors.";
-            } else if (playerSelection == "paper"){
-                result = "You Lose! Scissors beats paper.";
-            } else if (playerSelection == "scissors"){
-                result = draw;
-            } else {
-                result = "Oops...can you try again?";
-            }
-            break;
-    }
-    return result;
-}
-
-// Function to simulate 5 rounds of RPS
-function game(){
-    let playerScore = 0;
-    let computerScore= 0;
-    let msg = "";
-    let msg_fin = "";
+// Function to give game outcomes for user input
+function playRound(playSel){
+    // randomize computer selection
+    let compSel = computerPlay();
     
-    // loop to play 5 games
-    for (var i=1;i<6;i++){
-        let playerSelection = prompt("Choose RPS!").toLowerCase();
-        let computerSelection = computerPlay();
-        result = playRound(playerSelection,computerSelection);
-        console.log("Round " + i + ": " + result);
+    // game scenarios
 
-    // Keep track of digital score
-        if(result.includes("Win")){
-            playerScore += 1;
-            msg = "You win this round!";
-        } else if (result.includes("Lose")){
-            computerScore += 1;
-            msg = "You lose this round :(";
-        } else {
-            msg = "Draw!"
-        }
+    // player wins
+    if ((playSel === "Rock" && compSel === "Scissors") || (playSel === "Scissors" && compSel === "Paper") ||  (playSel === "Paper" && compSel === "Rock")){
+        playScore ++;
+        log.textContent = "You Win! " + playSel + " beats " + compSel;
+        console.log("You Win! " + playSel + " beats " + compSel);
+    } 
+    // computer wins
+    else if ((playSel === "Paper" && compSel === "Scissors") || (playSel === "Rock" && compSel === "Paper") || (playSel === "Scissors" && compSel === "Rock")){
+        compScore ++;
+        log.textContent = "You Lose! " + compSel + " beats " + playSel;
+        console.log("You Lose! " + compSel + " beats " + playSel);
+    } 
+    // draw scenario
+    else if (playSel === compSel){
+        log.textContent = "You chose: " + playSel + " -- Computer chose: " + compSel + ", it's a DRAW! Try again!";
+        console.log("You chose: " + playSel + " -- Computer chose: " + compSel + ", it's a DRAW! Try again!");
     }
 
-    // Calculate final outcome message
-    if (playerScore > computerScore){
-        msg_fin = "You won the series!"
-    }   else if (computerScore > playerScore){
-        msg_fin = "You lost the series."
-    }   else{
-        msg_fin = "Draw...really?"
+    //end game logic check if either user/comp reached 5 wins; log a message and disable buttons
+    if (playScore == 5){
+        result.textContent = 'You beat the machine! You Win!';
+        result.style.color = "green";
+        container.insertBefore(result,rbtn);
+        rbtn.disabled = true; 
+        pbtn.disabled = true; 
+        sbtn.disabled = true; 
+
+    } else if (compScore == 5){
+        result.textContent = 'Not today friend.  You Lose!';
+        result.style.color = "red";
+        container.insertBefore(result,rbtn);
+        rbtn.disabled = true; 
+        ptbn.disabled = true; 
+        sbtn.disabled = true; 
     }
-    // display scores with message
-console.log("Your score: " + playerScore + "\ncomputerScore:" + computerScore + "\n" + msg_fin);
+
 }
+
+//event listeners for choice selection followed by score logging
+let scoreboard = document.querySelector('#scoreboard');
+let pScore = document.createElement('h3');
+
+var rbtn = document.querySelector("#rbtn");
+rbtn.addEventListener('click', function() {
+    playRound("Rock");
+    pScore.textContent = "YOU (" + playScore + ") - Computer (" + compScore + ")";
+
+});
+var pbtn = document.querySelector("#pbtn");
+pbtn.addEventListener('click', function() {
+    playRound("Paper");
+    pScore.textContent = "YOU (" + playScore + ") - Computer (" + compScore + ")";
+
+});
+var sbtn = document.querySelector("#sbtn");
+sbtn.addEventListener('click', function() {
+    playRound("Scissors");
+    pScore.textContent = "YOU (" + playScore + ") - Computer (" + compScore + ")";
+});
+
+scoreboard.appendChild(pScore);
